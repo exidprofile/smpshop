a3d12a9ef3cd7344fdea187b7c5965294a3c0f75
Thuong Le <levienthuong@gmail.com>
2017-04-08 02:44:33 +0700

    First Commit 5

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,8 +24,8 @@ class CreateUsersTable extends Migration
             $table->string('avatar')->default('default.jpg');
             $table->tinyInteger('level')->default(3);
             $table->rememberToken();
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->string('updated_at')->nullable()->default(DB::raw('NULL on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->nullable()->default(0);
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
