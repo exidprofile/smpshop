a678721dacd1a5a5f883f0e6bc7a10c6b64b8845
Thuong Le <levienthuong@gmail.com>
2017-04-08 09:05:03 +0700

    Test

--- app/Http/Controllers/smpshop/IndexController.php
+++ app/Http/Controllers/smpshop/IndexController.php
@@ -12,6 +12,7 @@ use App\Http\Requests\ContactFormRequest;
 class IndexController extends Controller
 {
     public function index(){
+        return "Hello";
         $theme = FeaturedSlider::all();
         $products = Product::all();
         // dd($theme);
